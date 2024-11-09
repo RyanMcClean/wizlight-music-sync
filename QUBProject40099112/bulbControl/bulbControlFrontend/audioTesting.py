f036bd97d56e98891f3e81685e9c49c1d815184c
Ryan <ryan1328@hotmail.com>
2024-11-09 13:57:29 +0000

    More portable linux

--- QUBProject40099112/bulbControl/bulbControlFrontend/audioTesting.py
+++ QUBProject40099112/bulbControl/bulbControlFrontend/audioTesting.py
@@ -1,17 +1,17 @@
 import pyaudiowpatch as pyaudio
-from src.stream_analyzer import Stream_Analyzer
+from Realtime_PyAudio_FFT.src.stream_analyzer import Stream_Analyzer
 from time import sleep
 
 
-# p = pyaudio.PyAudio()
+p = pyaudio.PyAudio()
 
-# print(p.get_default_wasapi_loopback())
+print(p.get_default_wasapi_loopback())
 
-# sleep(20)
+sleep(20)
 
 ear = Stream_Analyzer(
                     device = 10,                    # Pyaudio (portaudio) device index, defaults to first mic input
-                    rate   = 48000,                  # Audio samplerate, None uses the default source settings
+                    rate   = 48000,                 # Audio samplerate, None uses the default source settings
                     FFT_window_size_ms  = 60,       # Window size used for the FFT transform
                     updates_per_second  = 500,      # How often to read the audio stream for new data
                     smoothing_length_ms = 50,       # Apply some temporal smoothing to reduce noisy features
@@ -22,7 +22,7 @@ ear = Stream_Analyzer(
                     window_ratio = 0                # Float ratio of the visualizer window. e.g. 24/9
                     )
 
-# sleep(20)
+sleep(20)
 
 count = 0
 while count < 1000:
---
+++ QUBProject40099112/bulbControl/bulbControlFrontend/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="Realtime_PyAudio_FFT", packages = find_packages())
