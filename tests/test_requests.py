226917faf21085c0117a0a33eeee2b087356dd45
Ryan <ryan1328@hotmail.com>
2024-12-05 00:11:42 +0000

    Updates requirements and one test

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,10 +1,12 @@
 import pytest
-from django.urls import reverse
-from django.test import Client
+import requests
+from time import sleep
 
 
+@pytest.mark.live_server
 @pytest.mark.django_db
-def test_homepage():
-    c = Client()
-    response = c.get("/")
+def test_index(live_server):
+    sleep(20)
+    url = live_server.url
+    response = requests.get(live_server.url)
     assert response.status_code == 200
