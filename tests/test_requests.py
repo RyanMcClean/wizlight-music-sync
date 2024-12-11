9f1f35d6f1efa3127da494cdf613e0aee00e3023
Ryan <ryan1328@hotmail.com>
2024-12-11 11:04:29 +0000

    Added more checks to tests and a new test for querying the bulb

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -17,32 +17,38 @@ class IndexClientTest(TestCase):
         self.assertTemplateUsed(response, "index.html")
 
     def test_post_index(self):
-        response = self.client.post("/", {"discover": ""})
+        response = self.client.post("/", {"discover": ""}, "application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["errorMessage"], bulbDiscoveryErrorMessage)
 
     def test_bulb_registration_form(self):
         form_data = {"bulbIp": localIp}
-        response = self.client.post("/", form_data)
+        response = self.client.post("/", form_data, "application/json")
         self.assertEqual(response.status_code, 200)
 
 
 class BulbFunctionsTest(TestCase):
+    def test_query_bulb(self):
+        post_data = {"ip": localIp}
+        response = self.client.post("/queryBulb/", post_data, "application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(json.loads(response.content.decode("utf-8")), bulbQueryErrorMessage)
+
     def test_toggle_bulb(self):
         post_data = {"ip": localIp}
-        response = self.client.post("/toggleBulb/", post_data)
+        response = self.client.post("/toggleBulb/", post_data, "application/json")
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(json.loads(response.content.decode("utf-8")), bulbQueryErrorMessage)
 
     def test_color_bulb(self):
         post_data = {"ip": localIp, "r": "1", "g": "1", "b": "1", "brightness": "1"}
-        response = self.client.post("/colorBulb/", post_data)
+        response = self.client.post("/colorBulb/", post_data, "application/json")
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(json.loads(response.content.decode("utf-8")), bulbQueryErrorMessage)
 
     def test_toggle_bulb_invalid_POST(self):
         post_data = {}
-        response = self.client.post("/toggleBulb/", post_data)
+        response = self.client.post("/toggleBulb/", post_data, "application/json")
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, "404.html")
 
