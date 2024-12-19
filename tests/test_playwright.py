8e6824c0c87312673c5836da65f9020699978934
Ryan <ryan1328@hotmail.com>
2024-12-19 00:09:37 +0000

    Fixed color picker js, added more playwright tests

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1,5 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 import os
 
 
@@ -20,9 +20,13 @@ class PlaywrightTests(StaticLiveServerTestCase):
     def test_load_index(self):
         chromiumPage = self.chromium.new_page()
         chromiumPage.goto(f"{self.live_server_url}")
-        chromiumFindButton = chromiumPage.locator("#find-bulbs-button")
-        chromiumFindButton.wait_for()
+        chromiumPage.locator("#find-bulbs-button").wait_for()
         self.assertEqual(chromiumPage.title(), "Bulb Bop")
+        expect(chromiumPage.locator("#main-title")).to_be_visible()
+        expect(chromiumPage.locator("#find-bulbs-button")).to_be_visible()
+        expect(chromiumPage.locator("#home-link")).to_be_visible()
+        expect(chromiumPage.locator("#faq-link")).to_be_visible()
+        expect(chromiumPage.locator("#about-link")).to_be_visible()
 
     def test_load_discover(self):
         chromiumPage = self.chromium.new_page()
@@ -30,3 +34,41 @@ class PlaywrightTests(StaticLiveServerTestCase):
         chromiumFindButton = chromiumPage.locator("#find-bulbs-button")
         chromiumFindButton.wait_for(timeout=0, state="attached")
         self.assertEqual(chromiumPage.title(), "Bulb Bop")
+        expect(chromiumPage.locator("#main-title")).to_be_visible()
+        expect(chromiumPage.locator("#find-bulbs-button")).to_be_visible()
+        expect(chromiumPage.locator("#home-link")).to_be_visible()
+        expect(chromiumPage.locator("#faq-link")).to_be_visible()
+        expect(chromiumPage.locator("#about-link")).to_be_visible()
+
+    def test_from_index_to_discover(self):
+        chromiumPage = self.chromium.new_page()
+        chromiumPage.goto(f"{self.live_server_url}")
+        chromiumFindButton = chromiumPage.locator("#find-bulbs-button")
+        chromiumFindButton.wait_for(timeout=0, state="attached")
+        chromiumFindButton.click()
+        chromiumFindButton.wait_for(timeout=0, state="attached")
+        url = chromiumPage.url
+        print(url)
+        self.assertIn("discover", url)
+        self.assertEqual(chromiumPage.title(), "Bulb Bop")
+        expect(chromiumPage.locator("#main-title")).to_be_visible()
+        expect(chromiumPage.locator("#find-bulbs-button")).to_be_visible()
+        expect(chromiumPage.locator("#home-link")).to_be_visible()
+        expect(chromiumPage.locator("#faq-link")).to_be_visible()
+        expect(chromiumPage.locator("#about-link")).to_be_visible()
+
+    def test_from_discover_to_index(self):
+        chromiumPage = self.chromium.new_page()
+        chromiumPage.goto(f"{self.live_server_url}/discover")
+        chromiumPage.locator("#find-bulbs-button").wait_for(timeout=0, state="attached")
+        chromiumPage.locator("#home-link").click()
+        chromiumPage.locator("#find-bulbs-button").wait_for(timeout=0, state="attached")
+        url = chromiumPage.url
+        print(url)
+        self.assertNotIn("discover", url)
+        self.assertEqual(chromiumPage.title(), "Bulb Bop")
+        expect(chromiumPage.locator("#main-title")).to_be_visible()
+        expect(chromiumPage.locator("#find-bulbs-button")).to_be_visible()
+        expect(chromiumPage.locator("#home-link")).to_be_visible()
+        expect(chromiumPage.locator("#faq-link")).to_be_visible()
+        expect(chromiumPage.locator("#about-link")).to_be_visible()
